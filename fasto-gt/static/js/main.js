860ede88ebec57d5174da4786c2e6a52dbeabf5f
AlexKa <x01z@mail.ru>
2018-12-31 17:15:56 +0300

    fasto

--- fasto-gt/static/js/main.js
+++ fasto-gt/static/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-$(".lang").click(function() {
+$(".lang").click(function () {
     $(".lang__switch").toggleClass("lang__switch_rus");
     $(".lang__span").toggleClass("lang__span_selected");
 });
@@ -15,10 +15,47 @@ $("#menu").on("click", "a", function (event) {
     }, 600);
 });
 
+// hamgurger;
+var hamburger = $(".hamburger");
+hamburger.click(function () {
+    hamburger.toggleClass("is-active");
+    // $(".menu").fadeToggle();
+    $(".menu").toggleClass("menu_active");
+
+    // disable scrolling when the menu is active
+    if (hamburger.hasClass("is-active")) {
+
+        var winScrollTop = $(window).scrollTop();
+        $(window).bind("scroll", function () {
+            $(window).scrollTop(winScrollTop);
+        });
+    } else {
+        $(window).off("scroll");
+    }
+})
+
+$(".menu__link").click(function () {
+    $(window).off("scroll");
+    hamburger.toggleClass("is-active");
+    $(".menu").toggleClass("menu_active");
+})
+
 // slick slaider
 $('.testimonial').slick({
     infinite: true,
     fade: true,
     prevArrow: "<button type='button' class='slick-prev pull-left'></button>",
     nextArrow: "<button type='button' class='slick-next pull-right'></button>"
+});
+
+// open and close project
+var projects = $(".projects__btn");
+projects.click(function () {
+    console.log(1);
+    if (projects.text() == "All projects") {
+        projects.text("close");
+    } else {
+        projects.text("All projects");
+    }
+    $(".projects__link:nth-child(n+4)").fadeToggle("200");
 });
